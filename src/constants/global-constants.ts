b538bd5491a9eb67c872fcdc2272a38af843b6ab
Vieri3 <egordohlyj4@gmail.com>
2026-03-02 08:59:31 +0300

    корректировка названий файлов

---
+++ src/constants/global-constants.ts
@@ -0,0 +1,11 @@
+export const dataDate = { date: 20, month: 1, year: 2026 };
+
+export const dataWeather = { temperature: -2, precipitation: 'Снег' };
+
+export const dataCounters = { countRandomEvents: 3, countPatrolling: 2 };
+
+export const enum EDataRandomEventsPosition {
+    INVENTORY = 'inventory',
+    PEOPLE = 'people',
+    FARM = 'farm'
+}
