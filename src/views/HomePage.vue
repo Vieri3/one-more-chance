f3517843ba170e8d260942ee541f992e02577ea0
Vieri3 <egordohlyj4@gmail.com>
2026-03-14 19:57:46 +0300

    стилизация страницы homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,8 +6,10 @@ import { useRouter } from 'vue-router';
 // функция при загрузке страницы меняет картинки background
 onMounted(() => {
     const RAND_NUM = Math.floor(Math.random() * 5);
-    const bgElement = document.querySelector<HTMLElement>('.start-bg');
-    if (bgElement) bgElement.style.backgroundImage = `url('./home-page/start-bg-${RAND_NUM}.png')`;
+    const bg = document.querySelector<HTMLElement>('.bg');
+    const bgSubstrate = document.querySelector<HTMLElement>('.bg-substrate');
+    if (bg) bg.style.backgroundImage = `url('./home-page/start-bg-${RAND_NUM}.png')`;
+    if (bgSubstrate) bgSubstrate.style.backgroundImage = `url('./home-page/start-bg-${RAND_NUM}.png')`;
 });
 // Используем для перехода на другие страницы в js
 const router = useRouter();
@@ -35,25 +37,25 @@ const goToGamePage = () => router.replace('/location');
 </script>
 
 <template>
-    <div class="w-200 h-150 flex justify-center mx-auto">
-        <div class="start-bg">
+    <div class="bg-substrate">
+        <div class="bg">
             <div class="flex flex-col mt-30 *:my-2 text-white text-xl ease duration-800">
                 <button
-                    class="hover:font-bold hover:text-2xl cursor-pointer"
+                    class="btn-home-page"
                     @click="goToNewGame"
                 >НОВАЯ ИГРА</button>
                 <!--если есть в localstorage то появляется кнопка продолжить-->
                 <button
                     v-if="loadGame != null"
-                    class="hover:font-bold hover:text-2xl cursor-pointer"
+                    class="btn-home-page"
                     @click="goToGamePage"
                 >ПРОДОЛЖИТЬ</button>
                 <button
-                    class="hover:font-bold hover:text-2xl cursor-pointer"
+                    class="btn-home-page"
                     @click="isModalSettingOpen = !isModalSettingOpen"
                 >НАСТРОЙКИ</button>
                 <button
-                    class="hover:font-bold hover:text-2xl cursor-pointer"
+                    class="btn-home-page"
                     @click="isModalAboutOpen = !isModalAboutOpen"
                 >О НАС</button>
             </div>
@@ -63,12 +65,12 @@ const goToGamePage = () => router.replace('/location');
     <!--модальное окно настроек-->
     <div
         v-if="isModalSettingOpen"
-        class="w-full h-full bg-black/30 backdrop-blur-md flex items-center justify-center z-10 fixed top-0 left-0"
+        class="modal-home-page text-modal-home-page"
     >
         <div class="bg-white/80 border-2 rounded border-amber-500 p-4 flex items-center justify-center flex-col w-1/3">
-            <h1 class="text-center text-black text-2xl font-bold mb-3">НАСТРОЙКИ ИГРЫ</h1>
+            <h1 class="text-center text-black font-black mb-3">НАСТРОЙКИ ИГРЫ</h1>
             <div>
-                <h2 class="text-black">Выбор языка игры</h2>
+                <h3 class="text-black">Выбор языка</h3>
                 <ul class="text-black">
                     <li>English</li>
                     <li>Polski</li>
@@ -85,10 +87,10 @@ const goToGamePage = () => router.replace('/location');
     <!--модальное окно О нас-->
     <div
         v-if="isModalAboutOpen"
-        class="w-full h-full bg-black/30 backdrop-blur-md flex items-center justify-center z-10 fixed top-0 left-0"
+        class="modal-home-page text-modal-home-page"
     >
-        <div class="bg-white/80 border-2 rounded border-amber-500 p-4 flex items-center justify-center flex-col w-1/3">
-            <h1 class="text-center text-black text-2xl font-bold mb-3">О НАС</h1>
+        <div class="bg-white/80 border-2 rounded border-amber-500 p-4 flex items-center justify-center flex-col w-1/3 ">
+            <h1 class="text-center text-black font-black mb-3">О НАС</h1>
             <div>
                 <p class="text-black">
                     Лишь на энтузиазме Чуб А. и Новик Ф. делают эту игру!
@@ -104,10 +106,10 @@ const goToGamePage = () => router.replace('/location');
     <!--модальное окно когда есть позиция в меню продолжение, но ты жмешь НОВАЯ ИГРА-->
     <div
         v-if="isModalNewGame"
-        class="w-full h-full bg-black/30 backdrop-blur-md flex items-center justify-center z-10 fixed top-0 left-0"
+        class="modal-home-page text-modal-home-page"
     >
         <div class="bg-white/80 border-2 rounded border-amber-500 p-4 flex items-center justify-center flex-col w-1/3">
-            <h1 class="text-center text-red-500 text-2xl font-bold mb-3">ВНИМАНИЕ</h1>
+            <h1 class="text-center text-red-500 font-black mb-3">ВНИМАНИЕ</h1>
             <div>
                 <p class="text-black">
                     Весь сохранённый ранее прогресс будет утерян!!!
@@ -130,10 +132,18 @@ const goToGamePage = () => router.replace('/location');
 </template>
 
 <style scoped>
-.start-bg {
-    width: 100%;
-    height: auto;
-    border: 1px solid tomato;
+.bg-substrate {
+    display: flex;
+    justify-content: center;
+    width: 100vw;
+    height: 100dvh;
+    background-position: center;
+    background-size: cover;
+}
+
+.bg {
+    width: 600px;
+    height: 100dvh;
     background-position: center;
     background-size: cover;
 }
